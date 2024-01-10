0f20535f0c9a311bed68fd1fae646e23e30dbd06
Randall Coding <randallcoding@protonmail.com>
2024-01-10 16:12:44 +0700

    Updates deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 acorn build -t ghcr.io/randall-coding/acorn/plausible && \
 acorn push ghcr.io/randall-coding/acorn/plausible && \
-acorn run -n plausible --memory=1Gi ghcr.io/randall-coding/acorn/plausible
+acorn run -n plausible ghcr.io/randall-coding/acorn/plausible
